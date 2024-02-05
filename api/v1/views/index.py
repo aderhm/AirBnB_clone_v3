e0cf88ff7581eca07e6b5dd86d30376e597d127d
Jo <69627271+jo-esign@users.noreply.github.com>
2024-02-05 13:54:18 +0100

    commit

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
-"""Module index.py"""
-
+'''Contains the index view for the API.'''
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 
-@app_views.route("/status", strict_slashes=False)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
-    """Returns API status"""
-    return jsonify({"status": "OK"})
+    """ Returns JSON """
+    return jsonify(status="OK")
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stat():
+    """returns the number of each objects by type"""
+    return jsonify(
+        amenities=storage.count('Amenity'),
+        cities=storage.count('City'),
+        places=storage.count('Place'),
+        reviews=storage.count('Review'),
+        states=storage.count('State'),
+        users=storage.count('User')
+    )
